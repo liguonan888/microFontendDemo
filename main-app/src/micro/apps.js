f4f924da0c03061c7c526ed80ab2f01f17af4c9c
李大帅 <@666.>
2022-03-19 16:59:42 +0800

    📖 document

--- main-app/src/micro/apps.js
+++ main-app/src/micro/apps.js
@@ -1,16 +1,15 @@
- const apps = [
+const apps = [
   {
-   name: "VueMicroApp",
-   entry: "//localhost:9000",
-   container: "#frame",
-   activeRule: "/vue",
+    name: "VueMicroApp",
+    entry: "//localhost:9000",
+    container: "#frame",
+    activeRule: "/vue",
   },
-     {
-         name: "ReactMicroApp",
-         entry: "//localhost:9001",
-         container: "#frame",
-         activeRule: "/react",
-     }
-
- ]
- export  default apps
+  {
+    name: "ReactMicroApp",
+    entry: "//localhost:9001",
+    container: "#frame",
+    activeRule: "/react",
+  },
+];
+export default apps;
